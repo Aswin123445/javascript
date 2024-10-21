9a71fdda21ad5d59d71a7c0c48c3e020f99ee389
java_learing <aswinsandeep4@gmail.com>
2024-10-21 16:10:43 +0530

    adding first javacript file

---
+++ java_basics/first/logic.js
@@ -0,0 +1,6 @@
+let sname = 'aswin sandeep';
+let age =15;
+let job = 'looking for job not yet';
+document.getElementById('p1').textContent=`name of is ${sname}`;
+document.getElementById('p2').textContent=age
+document.getElementById('p3').textContent=job
---
+++ java_basics/variable.js
@@ -0,0 +1,7 @@
+console.log("helo world");
+let age=25;
+let mark = 10.99;
+let gpa = 2.1
+console.log(`this is just testing ${age}`)
+
+
